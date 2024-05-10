d2690490dfb74f66561119c42e1677965b384922
Muaaz [Muhammadaziz] <muhammadaziz.erg.1229@gmail.com>
2024-05-10 19:50:59 +0500

    init

---
+++ src/main/java/com/example/courseprojectback/CourseProjectBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.courseprojectback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseProjectBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseProjectBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/courseprojectback/CourseProjectBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courseprojectback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseProjectBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
